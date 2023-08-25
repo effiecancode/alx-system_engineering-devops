c86203907f3c9546d005f2d175d7d54e11ee36fb
effiecancode <faitheffie25@gmail.com>
2023-08-25 22:47:26 +0300

    execute_a_command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,4 @@
-# execute pkill command to kill bashscript file killmenow
+# execute pkill command to kill bashscript file killmenow.
 exec { 'pkill':
   command => 'pkill -9 -f killmenow',
   path    => ['/usr/bin', '/usr/sbin', '/bin']
