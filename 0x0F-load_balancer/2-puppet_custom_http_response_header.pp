1bccacb909a02dac727c50f72ce47a56f415dda9
EffieNjoki <effienjoki@pop-os.localdomain>
2023-09-05 01:48:03 +0300

    puppet custom http response header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,9 @@
 # Automation: creates a custom HTTP header response with Puppet.
 
-exec { 'configure_custom_header':
-  command     => 'apt-get -y update && apt-get -y install nginx',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-available/default'],
-  require     => Package['nginx'],
-  provider    => shell,
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => template('my_module/nginx_default.erb'),
-  notify  => Exec['restart_nginx'],
-}
-
-exec { 'restart_nginx':
-  command     => 'service nginx restart',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-available/default'],
-  require     => Package['nginx'],
-  provider    => shell,
-}
-
