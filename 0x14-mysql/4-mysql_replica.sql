1ecc6a2b8f85eb4c4cf35fbe2a3597f97bdcec74
effiecancode <faitheffie25@gmail.com>
2023-09-20 02:42:08 +0300

    mysql replica

--- 0x14-mysql/4-mysql_replica.sql
+++ 0x14-mysql/4-mysql_replica.sql
@@ -1,10 +1,10 @@
 -- MySQL script to start replication on a replica server
 
 CHANGE MASTER TO
-    MASTER_HOST='52.87.155.27',
+    MASTER_HOST='34.232.72.158',
     MASTER_USER='replica_user',
-    MASTER_PASSWORD='replica_user_pwd',
-    MASTER_LOG_FILE='mysql-bin.000001',
+    MASTER_PASSWORD='effie254',
+    MASTER_LOG_FILE='mysql-bin.000002',
     MASTER_LOG_POS=154;
 
 START SLAVE;
