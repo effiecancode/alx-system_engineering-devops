e0c8eae84ce59e26bbd5d1f35bbe137afacdc0ab
effiecancode <faitheffie25@gmail.com>
2023-09-20 02:47:32 +0300

    mysql replica config

--- 0x14-mysql/4-mysql_replica.sql
+++ 0x14-mysql/4-mysql_replica.sql
@@ -1,11 +1,51 @@
--- MySQL script to start replication on a replica server
+# Copyright (c) 2014, 2023, Oracle and/or its affiliates.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2.0,
+# as published by the Free Software Foundation.
+#
+# This program is also distributed with certain software (including
+# but not limited to OpenSSL) that is licensed under separate terms,
+# as designated in a particular file or component or in included license
+# documentation.  The authors of MySQL hereby grant you an additional
+# permission to link the program and your derivative works with the
+# separately licensed software that they have included with MySQL.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License, version 2.0, for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
-CHANGE MASTER TO
-    MASTER_HOST='34.232.72.158',
-    MASTER_USER='replica_user',
-    MASTER_PASSWORD='effie254',
-    MASTER_LOG_FILE='mysql-bin.000002',
-    MASTER_LOG_POS=154;
+#
+# The MySQL  Server configuration file.
+#
+# For explanations see
+# http://dev.mysql.com/doc/mysql/en/server-system-variables.html
 
-START SLAVE;
-SHOW SLAVE STATUS\G;
+[mysqld]
+pid-file	= /var/run/mysqld/mysqld.pid
+socket		= /var/run/mysqld/mysqld.sock
+datadir		= /var/lib/mysql
+log-error	= /var/log/mysql/error.log
+# By default we only accept connections from localhost
+#bind-address	= 127.0.0.1
+# Disabling symbolic-links is recommended to prevent assorted security risks
+symbolic-links=0
+
+## Replica Dataase Configuration
+
+# Unique Server ID
+server-id	= 2
+
+# Binary Log file location
+log_bin		= /var/log/mysql/mysql-bin.log
+
+# Database to replicate
+binlog_do_db	= tyrell_corp
+
+# Replica's Relay Log file
+relay-log	= /var/log/mysql/mysql-relay-bin.log
