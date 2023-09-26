9e79f80e81e3708e12b159afa9e10c0f230c74ef
effiecancode <faitheffie25@gmail.com>
2023-09-26 03:41:52 +0300

    Fix tab

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,18 +12,23 @@ if __name__ == "__main__":
     user_info_url = f"{base_url}/users/{employee_id}"
     todos_url = f"{base_url}/todos?userId={employee_id}"
 
+    # HTTP to fetch data
     user_response = requests.get(user_info_url)
     todos_response = requests.get(todos_url)
 
+    # retrieve as json
     user_data = user_response.json()
     todos_data = todos_response.json()
 
+    # read the JSON
     employee_name = user_data.get("name")
     total_tasks = len(todos_data)
     completed_tasks = [task for task in todos_data if task["completed"]]
 
+    # print the data
     print(f"Employee {employee_name} is done with tasks"
           f"({len(completed_tasks)}/({total_tasks}):")
 
+    # return list of tasks
     for task in completed_tasks:
-        print(f"{task['title']}")
+        print(f"\t{task['title']}")
