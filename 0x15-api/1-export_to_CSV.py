8369a25f1f385a3602a14c7b938d41ef1256b92f
effiecancode <faitheffie25@gmail.com>
2023-09-26 04:03:24 +0300

    Export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Exports to-do list to CSV format."""
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+
+    # send HTTP request that retrieves data as JSON
+    user_info = requests.get(url + "users/{}".format(user_id)).json()
+    username = user_info.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    # write to csv using the csv module
+    with open(f"{user_id}.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+        ) for t in todos]
