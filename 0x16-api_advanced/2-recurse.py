e8c4b66f63342d88bbdced4a06ba951d7efc68ef
effiecancode <faitheffie25@gmail.com>
2023-10-11 05:26:18 +0300

    recurse it py-fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -28,9 +28,9 @@ def recurse(subreddit, hot_list=[], after=None):
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     response = requests.get(url,
-                       headers=headers,
-                       params=params,
-                       allow_redirects=False)
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
 
     if response.status_code != 200:
         return None
